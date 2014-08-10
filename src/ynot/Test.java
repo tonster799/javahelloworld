83be61daabf9a912cc6aad9961dcdf8351aa634e
tonster799 <tonster799@yahoo.com>
2014-08-10 10:38:45 -0700

    Update Test.java

--- src/ynot/Test.java
+++ src/ynot/Test.java
@@ -4,7 +4,7 @@ package ynot;
 public class Test {
 
     public static void main( String[] args) {
-        System.out.println( "Hello World");
+        System.out.println( "Hello World!!");
     }
 
 }
