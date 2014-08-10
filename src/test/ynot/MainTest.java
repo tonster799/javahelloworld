ac2a58d163cc8cb9216e8f068fd5f9f6a687cde6
Tony Lam <tonylam@yahoo-inc.com>
2014-08-10 16:41:54 -0700

    Redo structure

---
+++ src/test/ynot/MainTest.java
@@ -0,0 +1,14 @@
+package ynot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
